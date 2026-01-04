9dee201a0d8bceba59839d91b7269049a8bd2a9e
TheCodeBreakerK <kelvin.oliveira.dev@pm.me>
2026-01-04 04:08:53 +0000

    feat: update health check handler to use DTOs for response formatting

--- internal/handler/infra/health.go
+++ internal/handler/infra/health.go
@@ -3,7 +3,9 @@ package infra
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/TheCodeBreakerK/vanish-vault-api/internal/dto"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"go.uber.org/zap"
@@ -14,24 +16,27 @@ import (
 // @Description  Checks if the service and its dependencies (database) are operational.
 // @Tags         Infra
 // @Produce      json
-// @Success      200  {object}  map[string]string "Service is up and running"
-// @Failure      503  {object}  map[string]string "Service or dependencies are down"
+// @Success      200  {object}  dto.HealthCheckResponse "Service is up and running"
+// @Failure      503  {object}  dto.ErrorResponse "Service or dependencies are down"
 // @Router       /healthz [get]
 func NewHealthCheckHandler(log *zap.Logger, db *pgxpool.Pool) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		log.Info("Health check requested")
 
 		if err := db.Ping(c.Request.Context()); err != nil {
-			c.JSON(http.StatusServiceUnavailable, gin.H{
-				"status": "error",
-				"db": "disconnected",
+			c.JSON(http.StatusServiceUnavailable, dto.ErrorResponse{
+				Code:    http.StatusServiceUnavailable,
+				Status:  http.StatusText(http.StatusServiceUnavailable),
+				Message: "disconnected",
 			})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{
-			"status": "ok",
-			"service": "vanish-vault",
+		c.JSON(http.StatusOK, dto.HealthCheckResponse{
+			Code:    http.StatusOK,
+			Status:  http.StatusText(http.StatusOK),
+			TS:      time.Now(),
+			Service: "vanish-vault",
 		})
 	}
 }
